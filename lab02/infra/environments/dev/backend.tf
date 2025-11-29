9ae7e21284e840762de439bd5e822882b096b68d
Alfredo Tornero <e_aacarlos@stefanini.com>
2025-11-29 11:05:53 -0500

    formateo

--- lab02/infra/environments/dev/backend.tf
+++ lab02/infra/environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-tfstate"        # cámbialo a tu RG de state
-    storage_account_name = "scharfftstate"     # cámbialo a tu storage account
+    resource_group_name  = "rg-tfstate"    # cámbialo a tu RG de state
+    storage_account_name = "scharfftstate" # cámbialo a tu storage account
     container_name       = "tfstate"
     key                  = "dev.tfstate"
   }
